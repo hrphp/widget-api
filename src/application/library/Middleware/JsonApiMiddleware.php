8a9351ec071b98471feaa0c43f19a9bfd3e45ff1
guillermo.fisher <guillermo.fisher@dominionenterprises.com>
2015-01-30 22:05:18 -0500

    Libraries added.

---
+++ src/application/library/Middleware/JsonApiMiddleware.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the hrphp-widget-api package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Hrphp\Middleware;
+
+
+class JsonApiMiddleware
+{
+
+}
