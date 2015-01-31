9a72cb128f26bb71ecafb8807c6519444ea84843
guillermo.fisher <guillermo.fisher@dominionenterprises.com>
2015-01-30 22:14:42 -0500

    Added error route.

--- src/application/routes/errors.php
+++ src/application/routes/errors.php
@@ -5,3 +5,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+$app->error(function (\Exception $exception) {
+    $code = $exception->getCode() > 200 ? $exception->getCode() : 500;
+    throw new \Exception($exception->getMessage(), $code);
+});
