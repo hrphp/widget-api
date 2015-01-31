fefba100236bb419fb99d81e669e1935176b5f09
guillermo.fisher <guillermo.fisher@dominionenterprises.com>
2015-01-30 22:07:19 -0500

    PSR2 + docblock for app config

--- src/application/config/application.php
+++ src/application/config/application.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * This file is part of the hrphp-widget-api package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 require 'src/application/config/middleware.php';
 require 'src/application/config/routes.php';
