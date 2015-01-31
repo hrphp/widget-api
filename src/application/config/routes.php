8de0c053d1a40ea4bd34395d826cc6e7281e4302
guillermo.fisher <guillermo.fisher@dominionenterprises.com>
2015-01-30 22:06:20 -0500

    Added route includes.

---
+++ src/application/config/routes.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * This file is part of the hrphp-widget-api package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require 'src/application/config/errors.php';
+require 'src/application/config/widgets.php';
