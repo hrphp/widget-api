ab344a8bc5c4bb8548509e229f800a735c28882a
guillermo.fisher <guillermo.fisher@dominionenterprises.com>
2015-02-01 00:44:30 -0500

    Added SQL for database.

---
+++ conf/db.sql
@@ -0,0 +1,37 @@
+
+CREATE TABLE `widgets` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `color` varchar(45) NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `modifiedAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`,`color`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `widgets`
+(`name`,
+`color`,
+`createdAt`,
+`modifiedAt`)
+VALUES
+('My Favorite Widget',
+'black',
+CURRENT_TIMESTAMP,
+CURRENT_TIMESTAMP),
+('My Least Favorite Widget',
+'magenta',
+CURRENT_TIMESTAMP,
+CURRENT_TIMESTAMP),
+('Another Widget',
+'red',
+CURRENT_TIMESTAMP,
+CURRENT_TIMESTAMP),
+('A Random Widget',
+'blue',
+CURRENT_TIMESTAMP,
+CURRENT_TIMESTAMP),
+('Yet Another Widget',
+'green',
+CURRENT_TIMESTAMP,
+CURRENT_TIMESTAMP);
